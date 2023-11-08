5e02f42891bb96beb2a4b7c288ae58f580dc82ef
Goutam-04 <goutamnayak+github1@gmail.com>
2023-11-09 02:52:09 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProductList from './features/productList/ProductList';
+
+function App() {
+  return (
+    <div className="App">
+     <ProductList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/features/productList/productListSlice.js
@@ -0,0 +1,50 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { fetchCount } from './productListApi';
+
+const initialState = {
+  value: 0,
+  status: 'idle',
+};
+
+
+export  const incrementAsync = createAsyncThunk(
+  'productList/fetchCount',
+  async (amount) => {
+    const response = await fetchCount(amount);
+    // The value we return becomes the `fulfilled` action payload
+    return response.data;
+  }
+);
+
+export const productListSlice = createSlice({
+  name: 'productList',
+  initialState,
+  // The `reducers` field lets us define reducers and generate associated actions
+  reducers: {
+    increment: (state) => {
+
+      state.value += 1;
+    },
+
+  },
+  
+  
+  extraReducers: (builder) => {
+    builder
+      .addCase(incrementAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(incrementAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.value += action.payload;
+      });
+  },
+});
+
+export const { increment, decrement, incrementByAmount } = productListSlice.actions;
+
+
+export const selectCount = (state) => state.productList.value;
+
+
+export default productListSlice.reducer;
