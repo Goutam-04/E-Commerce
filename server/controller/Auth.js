6fb6a53561b70a287972ec99b885236f09258817
Goutam-04 <goutamnayak+github1@gmail.com>
2023-12-18 01:19:13 +0530

    connect auth with cookies

--- server/controller/Auth.js
+++ server/controller/Auth.js
@@ -6,7 +6,6 @@ const jwt = require('jsonwebtoken');
 
 exports.createUser = async (req, res) => {
   try {
-
     const salt = crypto.randomBytes(16);
     crypto.pbkdf2(
       req.body.password,
@@ -18,28 +17,38 @@ exports.createUser = async (req, res) => {
         const user = new User({ ...req.body, password: hashedPassword, salt });
         const doc = await user.save();
 
-        req.login(sanitizeUser(doc), (err) => {  // this also calls serializer and adds to session
+        req.login(sanitizeUser(doc), (err) => {
+          // this also calls serializer and adds to session
           if (err) {
             res.status(400).json(err);
           } else {
             const token = jwt.sign(sanitizeUser(doc), SECRET_KEY);
-            res.status(201).json(token);
+            res
+              .cookie('jwt', token, {
+                expires: new Date(Date.now() + 3600000),
+                httpOnly: true,
+              })
+              .status(201)
+              .json(token);
           }
         });
       }
     );
-
   } catch (err) {
     res.status(400).json(err);
   }
 };
 
 exports.loginUser = async (req, res) => {
-
-  res.json(req.user);
+  res
+    .cookie('jwt', req.user.token, {
+      expires: new Date(Date.now() + 3600000),
+      httpOnly: true,
+    })
+    .status(201)
+    .json(req.user.token);
 };
 
 exports.checkUser = async (req, res) => {
-  res.json({status:'success',user: req.user});
-
+  res.json({ status: 'success', user: req.user });
 };
